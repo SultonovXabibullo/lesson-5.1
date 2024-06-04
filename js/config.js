85074825892113cd1460f81c9306690b1ecafccc
SultonovXabibullo <sultonovxabibullo444@gmail.com>
2024-06-04 09:45:31 +0500

    jf

---
+++ js/config.js
@@ -0,0 +1,28 @@
+let day = 3;
+let dayName;
+switch (day) {
+    case 0:
+        dayName="Sunday";
+        break;
+    case 1:
+        dayName="Monday";
+        break;
+    case 2:
+        dayName="Tuesday"
+        break;
+    case 3:
+        dayName="Wednesday"
+        break;
+    case 4:
+        dayName="Thursday"
+        break;
+    case 5:
+        dayName="Friday"
+        break;
+    case 6:
+        dayName="Satuday"
+        break;
+    daFault:
+        dayName="Invalid day";
+}
+    console.log("Today is" + dayName);
